d6fae927fe04419b7811f3e6d9ae3b71afac5857
Matt Morgante <matthewmorgante@gmail.com>
2017-09-10 13:26:47 +0200

    speech detection

--- varletconst.js
+++
@@ -1,9 +0,0 @@
-mailalerton: function(){
-    $('.mail_alert').click(function(){
-            $.ajax({
-                url:  '/mailalert/activate/' + $(this).attr('data-value'),
-            });
-        }
-        $(this).toggleClass('icon-off');
-    });
-},
